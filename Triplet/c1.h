5d2a51956000ec2f70f660ddf93ae1fe9c2cd342
sheepmeng <sheepliangmeng@gmail.com>
2013-05-04 21:13:42 +0800

    new

--- Triplet/c1.h
+++ Triplet/c1.h
@@ -12,9 +12,10 @@
 # include <iostream.h> //cout,cin
 
 //函数结果状态代码
-# define FALSE 0
-# define OK 1
-# define ERROR 0
+# define FALSE       0
+# define TRUE        1
+# define OK          1
+# define ERROR       0
 # define INFEASIBLE -1
 
 typedef int Status;
